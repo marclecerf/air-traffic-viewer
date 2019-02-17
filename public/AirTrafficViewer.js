5d3fcd7549ddb5d0e2ed6e22161886ffe92ba559
Marc Lecerf <mrc.lcrf@gmail.com>
2019-02-17 01:15:31 -0800

    Aircraft prototype

--- public/AirTrafficViewer.js
+++ public/AirTrafficViewer.js
@@ -19,56 +19,82 @@ requirejs([], function () {
         wwd.addLayer(layers[l].layer);
     }
 
-    // Initialize the ducky vizrep.
-    var duckyModel = {'colladaScene': null, 'annotation': null};
-    var duckyLayer = new WorldWind.RenderableLayer("ducky");
-    var duckyPosition = new WorldWind.Position(45, -100, 3500);
-    var duckyColladaLoader = new WorldWind.ColladaLoader(duckyPosition);
-    duckyColladaLoader.init({'dirPath': 'public/'});
-    duckyColladaLoader.load('duck.dae', function (scene) {
-        duckyModel['colladaScene'] = scene;
-        var scale = 200;
-        duckyModel['colladaScene'].scale = scale;
-        var attrs = new WorldWind.AnnotationAttributes(null);
-        attrs.cornerRadius = 5;
-        attrs.backgroundColor = WorldWind.Color.BLACK;
-        attrs.drawLeader = true;
-        attrs.leaderGapWidth = 10;
-        attrs.leaderGapHeight = 100;
-        attrs.opacity = 1;
-        attrs.scale = 1;
-        attrs.width = 200;
-        attrs.height = 100;
-        attrs.textAttributes.color = WorldWind.Color.WHITE;
-        attrs.textAttributes.font.family = 'courier';
-        attrs.insets = new WorldWind.Insets(90, 10, 10, 54);
-        duckyModel['annotation'] = new WorldWind.Annotation(duckyPosition,
-                                                            attrs);
-        
-        duckyModel['annotation'].label = "Lorem Ipsum.";
-        duckyLayer.addRenderable(duckyModel['annotation']);
-        duckyLayer.addRenderable(duckyModel['colladaScene']);
-        console.log(duckyModel['annotation'])
-        wwd.addLayer(duckyLayer);
-    });
+    // Constructor for an Aircraft.
+    function Aircraft(callsign, position, scale, layer) {
+        self.callsign = callsign
+        self.colladaScene = null;
+        self.annotation = null;
+        self.colladaLoader = new WorldWind.ColladaLoader(position);
+        self.colladaLoader.init({'dirPath': 'public/'});
+        self.colladaLoader.load('duck.dae', function (scene) {
+            self.colladaScene = scene;
+            self.colladaScene.scale = scale;
+            var attrs = new WorldWind.AnnotationAttributes(null);
+            attrs.cornerRadius = 5;
+            attrs.backgroundColor = WorldWind.Color.BLACK;
+            attrs.drawLeader = true;
+            attrs.leaderGapWidth = 10;
+            attrs.leaderGapHeight = 100;
+            attrs.opacity = 1;
+            attrs.scale = 1;
+            attrs.width = 200;
+            attrs.height = 100;
+            attrs.textAttributes.color = WorldWind.Color.WHITE;
+            attrs.textAttributes.font.family = 'courier';
+            attrs.insets = new WorldWind.Insets(90, 10, 10, 54);
+            self.annotation = new WorldWind.Annotation(
+                self.colladaScene.position, attrs);
+            self.annotation.label = "Lorem Ipsum.";
+            layer.addRenderable(self.annotation);
+            layer.addRenderable(self.colladaScene);
+        });
+    }
 
+    Aircraft.prototype = {
+        set position(pos) {
+            if (self.colladaScene == null) return;
+            self.colladaScene.position = pos;
+            self.annotation.position = pos;
+            self.annotation.label =
+                "cs   :    " + self.callsign + "\n" +
+                "lat  : " + self.colladaScene.position.latitude.toFixed(6) + "\n" +
+                "lon  : " + self.colladaScene.position.longitude.toFixed(6) + "\n" +
+                "alt  :   " + self.colladaScene.position.altitude.toFixed(2) + "\n"
+        },
+
+        get position() {
+            if (self.colladaScene == null) return null;
+            else return self.colladaScene.position;
+        },
+
+        set scale(scale) {
+            if (self.colladaScene == null) return;
+            self.colladaScene.scale = scale;
+        }
+    }
+
+    var acLayer = new WorldWind.RenderableLayer("aircraft");
+    wwd.addLayer(acLayer);
+
+    var aircraftList = {};
     // Callbacks to handle socket.io messages from backend
     $(function () {
         var socket = io();
         socket.on('msg', function(msg) {
             var data = JSON.parse(msg);
-            duckyModel['colladaScene'].scale = data["scale"];
+            var cs = data["callsign"];
             var pos = new WorldWind.Position(data["lat_deg"],
                                              data["lon_deg"],
                                              data["alt_m"]);
-            duckyModel['colladaScene'].position = pos;
-            duckyModel['annotation'].position = pos;
-            duckyModel['annotation'].label =
-                "cs   :    " + data["callsign"] + "\n" +
-                "lat  : " + data["lat_deg"].toFixed(6) + "\n" +
-                "lon  : " + data["lon_deg"].toFixed(6) + "\n" +
-                "alt  :   " + data["alt_m"].toFixed(2) + "\n"
-            wwd.redraw()
+            var scale = data["scale"];
+            if (!(cs in aircraftList)) {
+                aircraftList[cs] = new Aircraft(cs, pos, scale, acLayer);
+            } else {
+                aircraftList[cs].position = pos;
+                aircraftList[cs].scale = scale;
+            }
+            console.log(aircraftList);
+            wwd.redraw();
         });
     });
 });
